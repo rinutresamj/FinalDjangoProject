ab832241a6b28182b7b654bc9bd592a396a5c360
Rinu Tresa M J <108607118+rinutresamj@users.noreply.github.com>
2023-08-08 16:00:54 +0530

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice,Enrollment,Submission
+
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class ChoiceInline(admin.TabularInline):  # Inline for editing choices within a question
+    model = Choice
+    extra = 3  # Number of extra choice fields to display
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]  # Embed ChoiceInline within QuestionAdmin
 
 
 class LessonInline(admin.StackedInline):
@@ -28,3 +35,9 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+
+# Register the Choice model if needed
+admin.site.register(Choice)
+admin.site.register(Enrollment)
+admin.site.register(Submission)
